baecfe7cc2ebba93dfdb02d4a1ea40b8834692f3
Artem <125490773+Dor112@users.noreply.github.com>
2024-12-22 18:10:42 +0300

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -2,94 +2,94 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main extends Object {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Bob", "Smith");
-        Student student2 = new Student("John", "Smith");
-        Teacher teacher1 = new Teacher("Bob", "Smith", new ArrayList<>());
-        Teacher teacher2 = new Teacher("John", "Smith", new ArrayList<>());
-        teacher1.addCourse("Math");
-        teacher1.addCourse("CS");
-        student1.setTutor(teacher1);
-        student1.setTutor(teacher2, teacher1);
-        System.out.println(teacher1);
 
-        System.out.println(student1);
-
-        List<People> peopleList = new ArrayList<>();
-        peopleList.add(student1);
-        peopleList.add(student2);
-        peopleList.add(teacher1);
-        peopleList.add(teacher2);
-
-
+        Student first = new Student("Artem", "Shendarov");
+        Teacher teacher = new Teacher("Pavel", "Panilov", new ArrayList<>());
+        teacher.addCourse("Methots of algoritmisation");
+        Teacher secteacher = new Teacher("Artem", "Nikolaev", new ArrayList<>());
+        secteacher.addCourse("Computer Science");
+        first.setTutor(teacher);
+        List<People> p = new ArrayList<>();
+        p.add(first);
+        p.add(teacher);
+        for (People pp: p) {
+            System.out.println(pp);
+        }
     }
 }
 
 abstract class People {
-    protected String name;
-    protected String surmame;
-    public People(String name, String surmame) {
-        this.name = name;
-        this.surmame = surmame;
+    String Name;
+    String Surname;
+
+    public People(String name, String surname) {
+        this.Surname = surname;
+        this.Name = name;
     }
 }
 
 class Student extends People {
-    private Teacher tutor;
 
-    public Student(String name, String surname){
+    private Teacher Tutor;
+
+    public Student(String name, String surname) {
         super(name, surname);
-    }
-
-    public void setTutor(Teacher tutor){
-        if (this.tutor != null) {
-            throw new IllegalStateException("Tutor has already been set");
-        }else{
-            this.tutor = tutor;
-        }
-    }
-
-    public void setTutor(Teacher newTutor, Teacher previousTutor){
-        if (this.tutor == previousTutor){
-            this.tutor = newTutor;
-        }
     }
 
     @Override
     public String toString() {
         return "Student{" +
-                "tutor=" + tutor +
-                ", name='" + name + '\'' +
-                ", surmame='" + surmame + '\'' +
+                "Tutor=" + Tutor +
+                ", Name='" + Name + '\'' +
+                ", Surname='" + Surname + '\'' +
                 '}';
+    }
+
+    public void setTutor(Teacher tutor) {
+        if (this.Tutor != null) {
+            throw new IllegalStateException("Student already have tutor" + this.Tutor);
+
+        } else {
+            this.Tutor = tutor;
+        }
     }
 }
 
-
-class Teacher  extends People{
-    private List<String> courses;
-    public Teacher(String name, String surmame, List<String> courses) {
-        super(name, surmame);
-        this.courses = new ArrayList<>(courses);
+class Teacher extends People{
+    private List<String> Courses;
+    public Teacher (String name, String surname, List<String> courses) {
+        super(name, surname);
+        this.Courses = new ArrayList<String>(courses);
     }
 
-    public void addCourse(String course){
-        courses.add(course);
+
+    public List<String> getCourse() {
+        return this.Courses;
     }
 
-    public void removeCourse(String course){
-        courses.remove(course);
+    public void setCourse(List<String> courses) {
+        this.Courses = courses;
+    }
+
+    public void addCourse (String course) {;
+        this.Courses.add(course);
+    }
+
+    public void removeCourse (String course) {
+        this.Courses.remove(course);
     }
 
     @Override
     public String toString() {
         return "Teacher{" +
-                "courses=" + courses +
-                ", name='" + name + '\'' +
-                ", surmame='" + surmame + '\'' +
+                "Courses=" + Courses +
+                ", Name='" + Name + '\'' +
+                ", Surname='" + Surname + '\'' +
                 '}';
     }
 }
